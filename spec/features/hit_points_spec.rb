d6d95bbecfaa3e099084a548b90085723f7ff782
Francesco <francescofacca@gmail.com>
2020-11-10 17:29:36 +0000

    DRY up code with helpers

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit("/")
-    fill_in :player_1_name, with: 'Fran'
-    fill_in :player_2_name, with: 'Chris'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Chris HP: 30'
   end
 end
